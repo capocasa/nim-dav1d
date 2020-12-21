c1bc4b425c0846d60940ecfeb9d1853c7dd14ce1
Carlo Capocasa <carlo@capocasa.net>
2020-12-21 12:08:52 +0100

    open and close library

--- dav1d.nim
+++ dav1d.nim
@@ -1,2 +1,19 @@
 
 import dav1d/wrapper
+
+type
+  InitError = object of IOError
+  DecoderObj = object
+    settings: ptr Settings
+    context: ptr Context
+  Decoder = ref DecoderObj
+
+proc cleanup(d: Decoder) =
+  close(d.context.addr)
+
+proc newDecoder(): Decoder =
+  new(result, cleanup)
+  default_settings(result.settings)
+  if 1 != open(result.context.addr, result.settings):
+    raise newException(InitError, "Failed to initialize Dav1d decoder")
+
